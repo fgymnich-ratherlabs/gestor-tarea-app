247def1d330bb6e4705c43cd7112fb250cc3dcd6
fgymnich-rather <fgymnich@ratherlabs.com>
2024-08-01 14:59:11 -0300

    add localStorage

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskList from './components/TaskList';
 import TaskForm from './components/TaskForm';
 import './styles.css';
 
 const App = () => {
-    const [tasks, setTasks] = useState([]);
+    // Inicializa el estado de tareas con datos del localStorage
+    const [tasks, setTasks] = useState(() => {
+        const savedTasks = localStorage.getItem('tasks');
+        return savedTasks ? JSON.parse(savedTasks) : [];
+        
+    });
 
+    //Efecto que guarda las tareas en localStorage cada vez que se actualizan
+    useEffect(() => {
+        localStorage.setItem('tasks',JSON.stringify(tasks));
+    }, [tasks]);
+    
     const addTask = (task) => {
-        setTasks([
-            ...tasks,
+        setTasks(prevTasks => [
+            ...prevTasks,
             { id: Date.now(), name: task.name }
         ]);
     };
 
     const deleteTask = (id) => {
-        setTasks(tasks.filter(task => task.id !== id));
+        setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
     };
 
     return (
