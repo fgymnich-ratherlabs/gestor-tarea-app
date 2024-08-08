04679b45b9b42cb24edc5018de95deaad99f64fc
fgymnich-rather <fgymnich@ratherlabs.com>
2024-08-08 20:46:27 -0300

    added unit tests

--- task-manager/src/components/TaskForm.js
+++ task-manager/src/components/TaskForm.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
 
 const TaskForm = ({ addTask, updateTask, currentTask }) => {
     const [name, setName] = useState('');
 
+    //Efecto para cargar el name en el form al editar tarea si es necesario
     useEffect(() => {
         if (currentTask) {
           setName(currentTask.name);
@@ -42,6 +45,13 @@ const TaskForm = ({ addTask, updateTask, currentTask }) => {
             </button>
         </form>
       );
+};
+
+// Define PropTypes for the component
+TaskForm.propTypes = {
+  addTask: PropTypes.func.isRequired,
+  updateTask: PropTypes.func.isRequired,
+  currentTask: PropTypes.func.isRequired,
 };
 
 export default TaskForm;
